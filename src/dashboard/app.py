8d018170ad001e8f565bbe79fbfffc47292d5ad4
Chuang Wang <chuangw.sde@gmail.com>
2021-01-21 21:22:01 -0800

    3nd try to fix import issue

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -5,11 +5,7 @@ from dash.dependencies import Input, Output
 import altair as alt
 import dash_bootstrap_components as dbc
 import pandas as pd
-
-import sys
-
-print("my_path", sys.path)
-import controls as ctrs
+from app.src.dashboard import controls as ctrs
 
 # Read in global data
 gapminder = pd.read_csv("data/processed/gapminder_processed.csv", parse_dates=["year"])
